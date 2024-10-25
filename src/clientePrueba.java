f4806b2573da31cdfd3b45523413dc5ade94bf55
Carlos-Gamrra554 <cargamtom@alu.edu.gva.es>
2024-10-25 08:27:43 +0200

    Actividad2

---
+++ src/clientePrueba.java
@@ -0,0 +1,8 @@
+public class clientePrueba {
+    public static void main(String[] args) {
+
+    clienteVip cliente = new clienteVip();
+    String nombre = "Nombre Apellido";
+        System.out.println("Nombre del cliente: " + cliente.getNombre());
+    }
+}
